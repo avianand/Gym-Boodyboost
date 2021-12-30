10f856c0d3b1d715149571e5f1cfa5f584885138
Abhisek Kumar Anand <anand5895@outlook.com>
2021-12-31 02:30:54 +0530

    changes
    some minor

--- src/Components/Carousel/sliderImage.js
+++ src/Components/Carousel/sliderImage.js
@@ -3,7 +3,7 @@ import { Button } from "@mui/material";
 export default [
   {
     title: `Bodyboost Gym`,
-    description: "This is the first slider Image of our carousel",
+    description: "Best Gym in Brajarajnagar & Belpahar",
     urls: "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   },
   {
